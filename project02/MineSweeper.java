4df2351b8fd5c151b7cb0ea6dd59a0a4b8133b8d
Ismail Ercedogan <ismail.ercedogan@ozu.edu.tr>
2021-06-12 21:20:14 +0300

    MineSweeper class is implemented

--- project02/MineSweeper.java
+++ project02/MineSweeper.java
@@ -1,10 +1,21 @@
 package project02;
-
+import javax.swing.*;
 public class MineSweeper {
 
-	public static void main(String[] args) {
 	
+		private static final int NUM_MINES=100;
+	    private static final int Sýze=20;
 
+
+	public static void main(String[] args){
+
+	    JFrame frame=new JFrame("Mine Sweeper | # of mines:"+ NUM_MINES);
+	    frame.add(new MineSweeperGUI(Sýze,Sýze,NUM_MINES));
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(1000,600);
+	    frame.setVisible(true);
+
+	
 	}
 
 }
