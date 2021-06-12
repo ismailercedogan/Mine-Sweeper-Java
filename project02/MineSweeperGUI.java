f9f5cea54a8fa1ad54cbfbf46dbf171aa699b4a4
Ismail Ercedogan <ismail.ercedogan@ozu.edu.tr>
2021-06-12 21:28:41 +0300

    MineGrid class is implemented

--- project02/MineSweeperGUI.java
+++ project02/MineSweeperGUI.java
@@ -1,5 +1,23 @@
 package project02;
+import javax.swing.*;
+import java.awt.*;
+public class MineSweeperGUI extends JPanel {
+	private  MineGrid grid;
 
-public class MineSweeperGUI {
+	public  MineSweeperGUI(int numRows, int numCols,int numMines){
 
-}
+	  grid=new MineGrid(numRows,numCols,numMines);
+	  setLayout(new GridLayout(numRows,numCols));
+
+	  for (int i=0;i<numRows;i++){
+	      for (int j=0;j<numCols;j++){
+	          JButton button= new JButton();
+	          add(button);
+	          button.addActionListener(new ButtonHandler(i,j,grid));
+
+	      }
+	  }
+
+	}
+	}
+
